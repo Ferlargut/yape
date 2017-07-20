b4bd7c7d7ef15175545cb34700ca0174b523dfbb
ferlargut <ferlargut@gmail.com>
2017-07-20 16:33:59 -0500

    Creando vistas y funcionalidad

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,68 @@
-var cargarPagina=function () {
+    
+    var codigotel = function () {
+        if(userData.code == $code){
+       alert("fer")
+    }else{
+        $.post('http://localhost:1388/api/registerNumber',{
+            "phone" : userData.phone, 
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
+    }  
+var cargarPagina=function () {    
     carrousel();   
-    $.when($("#telephone").val.length == 10).then(console.log("no se que hago"));
+    $("#check").click(validar);
+    $(".btn-validate").click(numVal);
+    $(".btn-validate").click(enviar);
+    validarCode();    
+    $("#boton-codigo").click(codigotel);    
 }
 var carrousel = function(){
     $('.slider').slider();
 }
 
+var validar = function () {
+    
+    if ($("#check").is(":checked")==true){
+        $(".btn-validate").removeClass("disabled")
+        
 
+    }else {
+        $(".btn-validate").addClass("disabled");
+    }
+}
+var numVal = function () {
+    var $tel=parseInt($("#telephone").val());
+    
+
+}
+var enviar = function () {
+    var $tel=$("#telephone").val();
+    $.post('http://localhost:1388/api/registerNumber',{
+        "phone":$tel,
+        "terms": true,
+    }).then(function (response) {
+        localStorage.setItem("jsonData", JSON.stringify(response));
+        var yapeUser = JSON.parse(localStorage.getItem("jsonData"));
+        if(yapeUser.success==true){
+            window.location.replace("/validarcode.html");
+        }
+        return yapeUser.data        
+    })    
+}
+var validarCode = function () {
+    var yapeUser = JSON.parse(localStorage.getItem("jsonData"));
+    var userData = yapeUser.data
+    var $code = $("#code").val();    
+
+    if(window.location.pathname == "/validarcode.html"){
+        alert(userData.code);
+        $("#numeroP").text(userData.phone);
+        
+    }
+ 
+     
+}
 
 $(document).ready(cargarPagina);
